2a119f0a488e737bef41a52ede86e31df857e38c
LouisLt <git@lutom.ski>
2024-05-30 09:05:58 +0200

    add bubble

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -10,8 +10,7 @@ class GamePage extends StatefulWidget {
   _GamePageState createState() => _GamePageState();
 }
 
-class _GamePageState extends State<GamePage>
-    with SingleTickerProviderStateMixin {
+class _GamePageState extends State<GamePage> with TickerProviderStateMixin {
   List<int> _diceValues = [1, 1, 1];
   int _currentPlayer = 1;
   int _player1Score = 0;
@@ -24,19 +23,22 @@ class _GamePageState extends State<GamePage>
   late Animation<double> _rotateAnimation;
   late Animation<double> _scaleAnimation;
   late Animation<double> _opacityAnimation;
+  late AnimationController _bubbleAnimationController;
+  late Animation<double> _bubbleAnimation;
+  final List<Offset> _bubblePositions = [];
 
   void _showRulesDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Règles du 421'),
+          title: const Text('Règles du Bubble 421'),
           content: const Text(
             'Le but du jeu est d\'être le premier joueur à atteindre 3 points.\n\n'
-            '- Chaque joueur lance 3 dés.\n'
+            '- Chaque joueur bubble-lance 3 dés.\n'
             '- Si vous obtenez la combinaison 4, 2 et 1 (dans n\'importe quel ordre), vous marquez un point.\n'
-            '- Si vous obtenez un ou plusieurs 6, vous devez les relancer.\n'
-            '- Le premier joueur à 3 points gagne la partie.',
+            '- Si vous obtenez un ou plusieurs 6, vous devez les bubble-relancer.\n'
+            '- Le premier joueur à 3 points bubble-gagne la partie.',
           ),
           actions: <Widget>[
             TextButton(
@@ -72,11 +74,22 @@ class _GamePageState extends State<GamePage>
         curve: Curves.easeIn,
       ),
     );
+    _bubbleAnimationController = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    );
+    _bubbleAnimation = Tween<double>(begin: 1, end: 0).animate(
+      CurvedAnimation(
+        parent: _bubbleAnimationController,
+        curve: Curves.linear,
+      ),
+    );
   }
 
   @override
   void dispose() {
     _animationController.dispose();
+    _bubbleAnimationController.dispose();
     super.dispose();
   }
 
@@ -128,6 +141,7 @@ class _GamePageState extends State<GamePage>
         _isWinningRound = true;
         if (_player1Score == 3 || _player2Score == 3) {
           _isGameOver = true;
+          _startBubbleAnimation();
         }
       });
       _animationController.forward(from: 0);
@@ -159,6 +173,7 @@ class _GamePageState extends State<GamePage>
       _currentPlayer = 1;
       _player1Score = 0;
       _player2Score = 0;
+      _bubblePositions.clear();
     });
   }
 
@@ -175,13 +190,35 @@ class _GamePageState extends State<GamePage>
     });
   }
 
+  void _startBubbleAnimation() {
+    _bubbleAnimationController.forward(from: 0).then((_) {
+      setState(() {
+        _bubblePositions.clear();
+      });
+      _bubbleAnimationController.reset();
+    });
+    _generateBubblePositions();
+  }
+
+  void _generateBubblePositions() {
+    final random = Random();
+    for (int i = 0; i < 50; i++) {
+      _bubblePositions.add(
+        Offset(
+          random.nextDouble() * MediaQuery.of(context).size.width,
+          random.nextDouble() * MediaQuery.of(context).size.height,
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final int numberOfPlayers = widget.numberOfPlayers;
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('421'),
+        title: const Text('Bubble 421'),
         centerTitle: true,
         actions: <Widget>[
           IconButton(
@@ -198,132 +235,155 @@ class _GamePageState extends State<GamePage>
             end: Alignment.bottomRight,
           ),
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Joueur $_currentPlayer',
-                style: const TextStyle(
-                    fontSize: 28,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              Row(
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: _diceValues.map((value) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: AnimatedBuilder(
-                      animation: _animationController,
-                      builder: (context, child) {
-                        return Transform.rotate(
-                          angle: (!_hasSixes || value == 6)
-                              ? _rotateAnimation.value
-                              : 0,
-                          child: ScaleTransition(
-                            scale: _isWinningRound
-                                ? _scaleAnimation
-                                : const AlwaysStoppedAnimation(1),
-                            child: Image.asset(
-                              'assets/images/inverted-dice-$value.png',
-                              width: 60,
-                              height: 60,
-                            ),
-                          ),
-                        );
-                      },
+                children: [
+                  Text(
+                    'Bubble-Joueur $_currentPlayer',
+                    style: const TextStyle(
+                        fontSize: 28,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: _diceValues.map((value) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: AnimatedBuilder(
+                          animation: _animationController,
+                          builder: (context, child) {
+                            return Transform.rotate(
+                              angle: (!_hasSixes || value == 6)
+                                  ? _rotateAnimation.value
+                                  : 0,
+                              child: ScaleTransition(
+                                scale: _isWinningRound
+                                    ? _scaleAnimation
+                                    : const AlwaysStoppedAnimation(1),
+                                child: Image.asset(
+                                  'assets/images/inverted-dice-$value.png',
+                                  width: 60,
+                                  height: 60,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  const SizedBox(height: 30),
+                  const Text(
+                    'Score :',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                  Text(
+                    'Bubble-Joueur 1 : $_player1Score',
+                    style: const TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  if (numberOfPlayers == 2)
+                    Text(
+                      'Bubble-Joueur 2 : $_player2Score',
+                      style: const TextStyle(fontSize: 20, color: Colors.white),
                     ),
-                  );
-                }).toList(),
-              ),
-              const SizedBox(height: 30),
-              const Text(
-                'Score :',
-                style: TextStyle(fontSize: 24, color: Colors.white),
-              ),
-              Text(
-                'Joueur 1 : $_player1Score',
-                style: const TextStyle(fontSize: 20, color: Colors.white),
-              ),
-              if (numberOfPlayers == 2)
-                Text(
-                  'Joueur 2 : $_player2Score',
-                  style: const TextStyle(fontSize: 20, color: Colors.white),
-                ),
-              const SizedBox(height: 40),
-              if (!_isGameOver && !_hasSixes && !_isWinningRound)
-                ElevatedButton(
-                  onPressed: _isAnimating ? null : _rollDice,
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 40, vertical: 20),
-                    textStyle: const TextStyle(fontSize: 18),
-                  ),
-                  child: const Text('Lancer les dés'),
-                ),
-              if (_hasSixes && !_isGameOver && !_isWinningRound)
-                ElevatedButton(
-                  onPressed: _isAnimating ? null : _rerollSixes,
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 40, vertical: 20),
-                    textStyle: const TextStyle(fontSize: 18),
-                  ),
-                  child: Text(
-                    _diceValues.where((value) => value == 6).length == 1
-                        ? 'Relancer le 6'
-                        : 'Relancer les 6',
-                  ),
-                ),
-              if (_isWinningRound && !_isGameOver)
-                ElevatedButton(
-                  onPressed: _isAnimating ? null : _continueGame,
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 40, vertical: 20),
-                    textStyle: const TextStyle(fontSize: 18),
-                  ),
-                  child: const Text('Continuer'),
-                ),
-              if (_isGameOver)
-                Column(
-                  children: [
-                    FadeTransition(
-                      opacity: _opacityAnimation,
-                      child: Text(
-                        _player1Score == 3
-                            ? 'Le joueur 1 a gagné !'
-                            : 'Le joueur 2 a gagné !',
-                        style:
-                            const TextStyle(fontSize: 24, color: Colors.white),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
+                  const SizedBox(height: 40),
+                  if (!_isGameOver && !_hasSixes && !_isWinningRound)
                     ElevatedButton(
-                      onPressed: _resetGame,
+                      onPressed: _isAnimating ? null : _rollDice,
                       style: ElevatedButton.styleFrom(
                         padding: const EdgeInsets.symmetric(
                             horizontal: 40, vertical: 20),
                         textStyle: const TextStyle(fontSize: 18),
                       ),
-                      child: const Text('Rejouer'),
+                      child: const Text('Lancer les dés'),
                     ),
-                  ],
-                ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _debugWinningMove,
-                style: ElevatedButton.styleFrom(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                  textStyle: const TextStyle(fontSize: 18),
-                ),
-                child: const Text('Debug - Coup gagnant'),
+                  if (_hasSixes && !_isGameOver && !_isWinningRound)
+                    ElevatedButton(
+                      onPressed: _isAnimating ? null : _rerollSixes,
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 20),
+                        textStyle: const TextStyle(fontSize: 18),
+                      ),
+                      child: Text(
+                        _diceValues.where((value) => value == 6).length == 1
+                            ? 'Relancer le 6'
+                            : 'Relancer les 6',
+                      ),
+                    ),
+                  if (_isWinningRound && !_isGameOver)
+                    ElevatedButton(
+                      onPressed: _isAnimating ? null : _continueGame,
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 20),
+                        textStyle: const TextStyle(fontSize: 18),
+                      ),
+                      child: const Text('Continuer'),
+                    ),
+                  if (_isGameOver)
+                    Column(
+                      children: [
+                        FadeTransition(
+                          opacity: _opacityAnimation,
+                          child: Text(
+                            _player1Score == 3
+                                ? 'Le Bubble-Joueur 1 a bubble-gagné !'
+                                : 'Le Bubble-Joueur 2 a bubble-gagné !',
+                            style: const TextStyle(
+                                fontSize: 24, color: Colors.white),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: _resetGame,
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 20),
+                            textStyle: const TextStyle(fontSize: 18),
+                          ),
+                          child: const Text('Rejouer'),
+                        ),
+                      ],
+                    ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _debugWinningMove,
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 20),
+                      textStyle: const TextStyle(fontSize: 18),
+                    ),
+                    child: const Text('Debug - Bubble-Coup gagnant'),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            if (_isGameOver)
+              AnimatedBuilder(
+                animation: _bubbleAnimationController,
+                builder: (context, child) {
+                  return Stack(
+                    children: _bubblePositions.map((position) {
+                      return Positioned(
+                        left: position.dx,
+                        top: position.dy * _bubbleAnimation.value,
+                        child: Image.asset(
+                          'assets/images/bubble.png',
+                          width: 30,
+                          height: 30,
+                        ),
+                      );
+                    }).toList(),
+                  );
+                },
+              ),
+          ],
         ),
       ),
     );
--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class MenuPage extends StatelessWidget {
+class MenuPage extends StatefulWidget {
   const MenuPage({super.key});
+
+  @override
+  _MenuPageState createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  final List<Bubble> _bubbles = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..repeat();
+    _generateBubbles();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _generateBubbles() {
+    for (int i = 0; i < 50; i++) {
+      _bubbles.add(Bubble());
+    }
+  }
+
+  void _popBubble(Offset position) {
+    setState(() {
+      _bubbles.removeWhere((bubble) {
+        final bubblePosition = Offset(bubble.x, bubble.y);
+        final distance = (bubblePosition - position).distance;
+        return distance < bubble.radius;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('421'),
+        title: const Text('Bubble 421'),
         centerTitle: true,
       ),
       body: Container(
@@ -18,39 +60,58 @@ class MenuPage extends StatelessWidget {
             end: Alignment.bottomRight,
           ),
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        child: GestureDetector(
+          onTapDown: (details) => _popBubble(details.localPosition),
+          child: Stack(
             children: [
-              const Icon(
-                Icons.casino,
-                size: 100,
-                color: Colors.white,
-              ),
-              const SizedBox(height: 40),
-              ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/game', arguments: 1);
+              AnimatedBuilder(
+                animation: _animationController,
+                builder: (context, _) {
+                  for (var bubble in _bubbles) {
+                    bubble.updatePosition(MediaQuery.of(context).size);
+                  }
+                  return CustomPaint(
+                    painter: BubblePainter(_bubbles),
+                    size: MediaQuery.of(context).size,
+                  );
                 },
-                icon: const Icon(Icons.person),
-                label: const Text('1 Joueur'),
-                style: ElevatedButton.styleFrom(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                  textStyle: const TextStyle(fontSize: 18),
-                ),
               ),
-              const SizedBox(height: 20),
-              ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/game', arguments: 2);
-                },
-                icon: const Icon(Icons.people),
-                label: const Text('2 Joueurs'),
-                style: ElevatedButton.styleFrom(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                  textStyle: const TextStyle(fontSize: 18),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.casino,
+                      size: 100,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(height: 40),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/game', arguments: 1);
+                      },
+                      icon: const Icon(Icons.person),
+                      label: const Text('1 Bubble-Joueur'),
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 15),
+                        textStyle: const TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/game', arguments: 2);
+                      },
+                      icon: const Icon(Icons.people),
+                      label: const Text('2 Bubble-Joueurs'),
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 15),
+                        textStyle: const TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
@@ -58,5 +119,61 @@ class MenuPage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class Bubble {
+  double x;
+  double y;
+  double radius;
+  double speed;
+  double direction;
+
+  Bubble({
+    this.x = 0,
+    this.y = 0,
+    this.radius = 0,
+    this.speed = 0,
+    this.direction = 0,
+  }) {
+    Random random = Random();
+    x = random.nextDouble() * 400;
+    y = random.nextDouble() * 800;
+    radius = random.nextDouble() * 20 + 10;
+    speed = random.nextDouble() * 2 + 1;
+    direction = random.nextDouble() * 2 * pi;
+  }
+
+  void updatePosition(Size screenSize) {
+    x += speed * cos(direction);
+    y += speed * sin(direction);
+
+    if (x < 0 || x > screenSize.width) {
+      direction = pi - direction;
+    }
+    if (y < 0 || y > screenSize.height) {
+      direction = -direction;
+    }
+  }
+}
+
+class BubblePainter extends CustomPainter {
+  final List<Bubble> bubbles;
+
+  BubblePainter(this.bubbles);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (var bubble in bubbles) {
+      final paint = Paint()
+        ..color = Colors.white.withOpacity(0.5)
+        ..style = PaintingStyle.fill;
+      canvas.drawCircle(Offset(bubble.x, bubble.y), bubble.radius, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
